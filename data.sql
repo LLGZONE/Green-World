d571a54e9b25a30116256ac6169acc82dc062df8
llgzone <985395976@qq.com>
2017-12-15 17:45:32 +0800

    add sql

--- data.sql
+++ data.sql
@@ -37,8 +37,17 @@ create table food (
 );
 
 create table bus (
+    id serial primary key,
     add_at timestamp default now(),
     user_id text not null unique,
-    foreign key (user_id) references users(uid),
-    img_dir text not null
+    img_dir text not null,
+    foreign key (user_id) references users(uid)
+);
+
+create table recycle (
+    id serial primary key,
+    user_id text not null unique,
+    recycle_date date not null,
+    recycle_time time not null，
+    foreign key (user_id) references users(uid)
 );
