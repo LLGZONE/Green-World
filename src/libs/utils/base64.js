04af25e5211d99634ffb75e126d9c92e2dbe8671
llgzone <985395976@qq.com>
2017-12-29 19:34:35 +0800

    convert base64 to img

---
+++ src/libs/utils/base64.js
@@ -0,0 +1,32 @@
+function decodeBase64Image(dataString) {
+  const matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+  let response = {}
+
+  if (matches.length !== 3) {
+    return new Error('Invalid input string')
+  }
+
+  response.type = matches[1]
+  response.data = new Buffer(matches[2], 'base64')
+
+  return response
+}
+
+function base64ToImageFile(dataString, fileDir) {
+  const imgBuffer = decodeBase64Image(dataString)
+  const crypto = require('crypto')
+  const seed = crypto.randomBytes(20)
+  const uniqueSHAStr = crypto
+    .createHash('sha1')
+    .update(seed)
+    .digest('hex')
+  const imageType = imgBuffer.type.match(/\/(.*?)$/)[1]
+
+  const imagePath = `${fileDir}image-${uniqueSHAStr}.${imageType}`
+
+  require('fs').writeFile(imagePath, imgBuffer.data, () => {
+    console.log('img into filedir :', fileDir)
+  })
+}
+
+module.exports = base64ToImageFile
