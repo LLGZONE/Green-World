cbaeca5bf38fd092944f974864723731fd99fc80
llg <985395976@qq.com>
2018-10-15 15:28:05 +0800

    init admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,46 +1,25 @@
-/*const router = require('koa-router')()
-const koaParser = require('koa-bodyparser')
-const crypto = require('crypto')
-const { ap, an, sk } = require('../libs/secrets')
-const { saveToRedis, expireToken, veryfiToken } = require('../redis')
+const router = require('koa-router')();
+const koaParser = require('koa-bodyparser');
+const crypto = require('crypto');
+const { ap, an, sk } = require('../libs/secrets');
 
 router.post('/', koaParser(), async (ctx, next) => {
-  const username = ctx.request.body.username
-  const password = ctx.request.body.password
+  const username = ctx.request.body.username;
+  const password = ctx.request.body.password;
 
-  console.log(username, password,ctx.request.body)
+  console.log(username, password, ctx.request.body);
   if (username !== an || password !== ap) {
     ctx.body = {
       error: 'username or password wrong'
-    }
-    return
+    };
+    return;
   }
-
-  const hashPassword = crypto
-    .createHmac('sha256', sk)
-    .update(ap)
-    .digest('hex')
-
-  const randomL = Math.floor(Math.random() * 40)
-  const randomR = randomL + 20
-
-  const userId = hashPassword.substring(randomL, randomR)
-  saveToRedis(userId)
 
   ctx.body = {
-    userId,
-  }
+    error: ''
+  };
 
-  await next()
-})
+  await next();
+});
 
-//logout
-router.delete('/', koaParser(), async (ctx, next) => {
-  ctx.status = await expireToken(ctx.headers)
-  await next()
-})
-
-router.post('/bonus/:userid', veryfiToken, )
-
-module.exports = router
-*/
+module.exports = router;
